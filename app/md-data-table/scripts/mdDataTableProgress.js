cfddca618028fb8b808e7783fbc503b9c7c5885d
Patryk Golabek <pgolabek@gmail.com>
2015-11-30 13:07:51 -0500

    fix for progress indicator scope

--- app/md-data-table/scripts/mdDataTableProgress.js
+++ app/md-data-table/scripts/mdDataTableProgress.js
@@ -1,27 +1,25 @@
 angular.module('md.data.table').directive('mdTableProgress', mdTableProgress);
 
 function mdTableProgress() {
-  'use strict';
-  
-  function postLink(scope, element, attrs, tableCtrl) {
-    
-    scope.getColumnCount = function () {
-      return tableCtrl.columns.length;
+    'use strict';
+
+    function postLink(scope, element, attrs, tableCtrl) {
+
+        tableCtrl.hideProgress = function () {
+            scope.showProgress = false;
+        };
+
+        tableCtrl.showProgress = function () {
+            scope.columnCount = tableCtrl.columns.length;
+            scope.showProgress = true;
+        };
+    }
+
+    return {
+        link: postLink,
+        require: '^mdDataTable',
+        replace: true,
+        templateUrl: 'templates.md-data-table-progress.html',
+        scope: true
     };
-    
-    tableCtrl.hideProgress = function () {
-      scope.showProgress = false;
-    };
-    
-    tableCtrl.showProgress = function () {
-      scope.showProgress = true;
-    };
-  }
-  
-  return {
-    link: postLink,
-    require: '^mdDataTable',
-    replace: true,
-    templateUrl: 'templates.md-data-table-progress.html'
-  };
 }
--- dist/md-data-table.js
+++ dist/md-data-table.js
@@ -581,29 +581,27 @@ mdDataTablePagination.$inject = ['$q'];
 angular.module('md.data.table').directive('mdTableProgress', mdTableProgress);
 
 function mdTableProgress() {
-  'use strict';
-  
-  function postLink(scope, element, attrs, tableCtrl) {
-    
-    scope.getColumnCount = function () {
-      return tableCtrl.columns.length;
+    'use strict';
+
+    function postLink(scope, element, attrs, tableCtrl) {
+
+        tableCtrl.hideProgress = function () {
+            scope.showProgress = false;
+        };
+
+        tableCtrl.showProgress = function () {
+            scope.columnCount = tableCtrl.columns.length;
+            scope.showProgress = true;
+        };
+    }
+
+    return {
+        link: postLink,
+        require: '^mdDataTable',
+        replace: true,
+        templateUrl: 'templates.md-data-table-progress.html',
+        scope: true
     };
-    
-    tableCtrl.hideProgress = function () {
-      scope.showProgress = false;
-    };
-    
-    tableCtrl.showProgress = function () {
-      scope.showProgress = true;
-    };
-  }
-  
-  return {
-    link: postLink,
-    require: '^mdDataTable',
-    replace: true,
-    templateUrl: 'templates.md-data-table-progress.html'
-  };
 }
 
 
@@ -1277,7 +1275,7 @@ angular.module('templates.md-data-table-progress.html', []).run(['$templateCache
   $templateCache.put('templates.md-data-table-progress.html',
     '<thead ng-if="showProgress">\n' +
     '  <tr>\n' +
-    '    <th colspan="{{getColumnCount()}}">\n' +
+    '    <th colspan="{{columnCount}}">\n' +
     '      <md-progress-linear md-mode="indeterminate"></md-progress-linear>\n' +
     '    </th>\n' +
     '  </tr>\n' +
